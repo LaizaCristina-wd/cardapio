d556e2055e533dc955e6009681c8de6aedb977c1
laiza <laizacristina2507@gmail.com>
2026-04-06 00:59:24 -0300

    Criando barra de navegação geral do app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
 <script setup>
+import { RouterLink, RouterView } from "vue-router"
 </script>
 
 <template>
 <div class="container py-4">
         <h1 class="text-center text-success mb-4">Bem vindo ao Controle de Produtos</h1>
+        <nav class="menu">
+
+      <RouterLink to="/">
+        Home
+      </RouterLink>
+
+      <RouterLink to="/cadastro">
+        Cadastro
+      </RouterLink>
+
+      <RouterLink to="/cardapio">
+        Cardápio
+      </RouterLink>
+
+    </nav>
+      <RouterView />
   </div>
-     <router-view />
+   
 </template>
 
-<style scoped></style>
+<style scoped>
+.titulo{
+  text-align:center;
+  margin-top:20px;
+  color:#16a34a;
+}
+
+.menu{
+  display:flex;
+  justify-content:center;
+  gap:20px;
+  margin:20px 0;
+}
+
+.menu a{
+  text-decoration:none;
+  color:#333;
+  font-weight:500;
+  padding:6px 12px;
+  border-radius:6px;
+}
+
+.menu a:hover{
+  background:#f0fdf4;
+  color:#16a34a;
+}
+
+.router-link-active{
+  background:#16a34a;
+  color:white;
+}
+
+</style>

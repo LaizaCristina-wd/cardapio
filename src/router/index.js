727b1a8db68af388a25942eaa408d1f628506037
laiza <laizacristina2507@gmail.com>
2026-04-13 17:03:15 -0300

    organização de código

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router"
-
 import HomeView from "../views/HomeView.vue"
 import CadastroView from "../views/CadastroView.vue"
 import ControleView from "../views/ControleView.vue"
